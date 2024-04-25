415bec215c413d2eea46a96603aed3d727be21ec
Joao Pedro Silveira <jpss06joao@gmail.com>
2024-04-25 07:54:14 -0300

    commit de isso

---
+++ src/Components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <h1 className="font-Beba text-5xl text-center pt-16">Avatar: O Caminho da Água</h1>
+  )
+}
---
+++ src/pages/Personagens.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+export default function Personagens() {
+  return (
+    <div>
+        <Header />
+        <h1 className='text-3xl font-bold text-center'>Personagens</h1>
+    </div>
+  )
+}
